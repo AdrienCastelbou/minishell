0f984b309f9bfd7519a471ecef7261cd490d38ed
AdrienCastelbou <acastelbou1@gmail.com>
2021-03-15 10:35:10 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <curses.h>
+#include <term.h>
+#include <unistd.h>
+#include "libft.h"
+
+int main(int argc, char **argv)
+{
+	char	buffer[4];
+	char	*tmp;
+	char	*input;
+	char	**splited_inputs;
+	int		size;
+
+	input = malloc(1);
+	*input = 0;
+	while ( !ft_strchr(input, '\n') && (size = read(STDIN_FILENO, buffer, 4)) > 0)
+	{
+		buffer[size] = 0;
+		tmp = input;
+		input = ft_strjoin(tmp, buffer);
+	}
+
+	splited_inputs = ft_split(input, ' ');
+	if (!ft_strchr(splited_inputs[0], '/'))
+	{
+		splited_inputs[0][3] = 0;
+		tmp = splited_inputs[0];
+		splited_inputs[0] = ft_strjoin("/bin/", tmp);
+		free(tmp);
+	}
+	execve(splited_inputs[0], splited_inputs, NULL);
+	int i;
+	i = -1;
+	while (splited_inputs[++i])
+		free(splited_inputs[i]);
+	free(splited_inputs);
+	free(input);
+}
